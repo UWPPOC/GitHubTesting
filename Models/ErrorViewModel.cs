9943d7640a4ebea271ce03121c4801e8224d40ab
Deepak Kumar <Deepak.Kumar03@libertymutual.com>
2025-08-19 12:55:45 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UWPTechLearningApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
